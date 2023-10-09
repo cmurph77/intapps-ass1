08edba6bb78e1c0a5fcbcc171d53106d94196312
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-09 19:35:06 +0100

    works for 3 hour forcast.

--- api_test.js
+++ api_test.js
@@ -3,12 +3,14 @@
 // 2 -- packing if(rain){  bringUmbrella = true}
 // 3 -- type of clothes - cold[<13], mild [13 - 23], hot[>23]
 
-const apiKey = '9d54b4134840423050e9a3f21b40dc15'; // Replace with your OpenWeatherMap API key
+const { raw } = require("body-parser");
 
+const apiKey = '9d54b4134840423050e9a3f21b40dc15'; // Replace with your OpenWeatherMap API key
 
 async function getWeatherForecast(city) {
     const count = 5;
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=${count}&appid=${apiKey}`;
+    let forecastData;
 
     try {
       const response = await fetch(apiUrl);
@@ -16,36 +18,55 @@ async function getWeatherForecast(city) {
         throw new Error('Network response was not ok');
       }
   
-      const forcastData = await response.json();
-      console.log(JSON.stringify(forcastData,null ,5));
-
+    forecastData = await response.json();
+    //console.log(JSON.stringify(forecastData,null ,5));
     } 
     catch (error) {
       console.error('Error fetching weather forecast data:', error);
     }
-  }
+
+    return forecastData;
+
+}
+
+function createForcastObj(raw_data){
+    const forecast = {
+        "city": raw_data.city.name,
+        "day_1" :{
+            "unix_dt": raw_data.list[0].dt,
+            "temp" : raw_data.list[0].main.temp,
+            "windspeed" : raw_data.list[0].wind.speed,
+
+        },
+        "day_2" :{
+            "unix_dt": raw_data.list[1].dt,
+            "temp" : raw_data.list[1].main.temp
+
+        }
+    }
+    return forecast;
+}
+
+function createForecastObject_Iterative(raw_data) {
+    const forecast = {
+      city: raw_data.city.name,
+    };
   
-  // Call the function to get the 5-day weather forecast
-  getWeatherForecast('New York');
+    for (let i = 0; i < 5; i++) {
+      const dayNumber = i + 1;
+      const unix_dt = raw_data.list[i].dt;
+      forecast[`day_${dayNumber}`] = { unix_dt };
+    }
+  
+    return forecast;
+  }
 
-// async function getCurrentWeather() {
+async function main(){
+    // Call the function to get the 5-day weather forecast
+    const returned_data = await getWeatherForecast('New York');
+    console.log(JSON.stringify(returned_data, null, 4)); 
+    const cleaned_data =  createForcastObj(returned_data);
+    console.log(JSON.stringify(cleaned_data, null, 4)); 
 
-//   try {
-//     const response = await fetch(apiUrl);
-//     //));
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-
-//     const weatherData = await response.json();
-
-//     console.log(JSON.stringify(weatherData,null ,3));
-//     const description = weatherData.weather[0].description;
-//     const temperature = weatherData.main.temp;
-
-//     console.log('Weather in New York City:', description);
-//     console.log('Temperature:', temperature, '°C');
-//   } catch (error) {
-//     console.error('Error fetching weather data:', error);
-//   }
-// }
+}
+main();
