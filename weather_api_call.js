564547bacfda208194602286f08e539b70e31c27
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-09 20:07:21 +0100

    added in sample cleaned data

--- weather_api_call.js
+++ weather_api_call.js
@@ -7,13 +7,39 @@ const { raw } = require("body-parser");
 
 const apiKey = '9d54b4134840423050e9a3f21b40dc15'; // Replace with your OpenWeatherMap API key
 
-async function getWeatherForecast(city) {
+async function getWeatherForecastThreeHour(city) {
     const count = 5;
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=${count}&appid=${apiKey}`;
     let forecastData;
 
     try {
       const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+    forecastData = await response.json();
+    //console.log(JSON.stringify(forecastData,null ,5));
+    } 
+    catch (error) {
+      console.error('Error fetching weather forecast data:', error);
+    }
+
+    return forecastData;
+
+}
+
+async function getWeatherForecastDaily(city) {
+    const count = 2;
+    const lat = 40.7128;
+    const long = 74.0060;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall/daily?q=dublin&cnt=${count}&appid=${apiKey}`;
+    let forecastData;
+
+    try {
+      const response = await fetch(apiUrl);
+      console.log('Response Status:', response.status); // Log the response status
+
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
@@ -47,26 +73,13 @@ function createForcastObj(raw_data){
     return forecast;
 }
 
-function createForecastObject_Iterative(raw_data) {
-    const forecast = {
-      city: raw_data.city.name,
-    };
-  
-    for (let i = 0; i < 5; i++) {
-      const dayNumber = i + 1;
-      const unix_dt = raw_data.list[i].dt;
-      forecast[`day_${dayNumber}`] = { unix_dt };
-    }
-  
-    return forecast;
-  }
 
 async function main(){
     // Call the function to get the 5-day weather forecast
-    const returned_data = await getWeatherForecast('New York');
+    const returned_data = await getWeatherForecastThreeHour('New York');
     console.log(JSON.stringify(returned_data, null, 4)); 
-    const cleaned_data =  createForcastObj(returned_data);
-    console.log(JSON.stringify(cleaned_data, null, 4)); 
+    // const cleaned_data =  createForcastObj(returned_data);
+    // console.log(JSON.stringify(cleaned_data, null, 4)); 
 
 }
 main();
