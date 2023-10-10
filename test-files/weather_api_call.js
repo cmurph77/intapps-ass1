e9591c922190422b5b0536637c66395371396439
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-10 17:45:44 +0100

    weather api call works and returns correct data

--- test-files/weather_api_call.js
+++ test-files/weather_api_call.js
@@ -1,39 +1,12 @@
-// required data for the api to return
-// 1 -- 5 day weather forcast - display as table with temp, windspeed and rainfall
-// 2 -- packing if(rain){  bringUmbrella = true}
-// 3 -- type of clothes - cold[<13], mild [13 - 23], hot[>23]
-
-const { raw } = require("body-parser");
 
 const apiKey = '9d54b4134840423050e9a3f21b40dc15'; // Replace with your OpenWeatherMap API key
 
-async function getWeatherForecastThreeHour(city) {
-    const count = 5;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=${count}&appid=${apiKey}`;
-    let forecastData;
-
-    try {
-      const response = await fetch(apiUrl);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-  
-    forecastData = await response.json();
-    //console.log(JSON.stringify(forecastData,null ,5));
-    } 
-    catch (error) {
-      console.error('Error fetching weather forecast data:', error);
-    }
-
-    return forecastData;
-
-}
-
+// this function takes a city as a parameter and makes call to api to get weather forcast for that city
 async function getWeatherForecastDaily(city) {
     const count = 2;
-    const lat = 40.7128;
-    const long = 74.0060;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall/daily?q=dublin&cnt=${count}&appid=${apiKey}`;
+    const long = 40.7128;
+    const lat = 74.0060;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,hourly,minutely,alerts&units=metric&appid=${apiKey}`;
     let forecastData;
 
     try {
@@ -45,41 +18,41 @@ async function getWeatherForecastDaily(city) {
       }
   
     forecastData = await response.json();
-    //console.log(JSON.stringify(forecastData,null ,5));
     } 
     catch (error) {
       console.error('Error fetching weather forecast data:', error);
     }
-
-    return forecastData;
+    return createForcastObj(forecastData);
 
 }
 
+// cleand up the forecast data
 function createForcastObj(raw_data){
-    const forecast = {
-        "city": raw_data.city.name,
-        "day_1" :{
-            "unix_dt": raw_data.list[0].dt,
-            "temp" : raw_data.list[0].main.temp,
-            "windspeed" : raw_data.list[0].wind.speed,
+  const forecast = {
+    "lat": raw_data.lat,
+    "lon": raw_data.lon,
+  };
 
-        },
-        "day_2" :{
-            "unix_dt": raw_data.list[1].dt,
-            "temp" : raw_data.list[1].main.temp
+  for (let i = 1; i <= 6; i++) {
+    forecast[`day_${i}`] = {
+        "unix_dt": raw_data.daily[i - 1].dt,
+        //"temp": raw_data.daily[i - 1].main.temp,
+       // "windspeed": raw_data.daily[i - 1].wind.speed,
+        //"weather-type": raw_data.daily[i - 1].weather.main,
+        //"weather-description": raw_data.daily[i - 1].weather.description,
+       // "rain": raw_data.daily[i - 1].rain
+    };
+  }
 
-        }
-    }
-    return forecast;
+  return forecast;
+
 }
 
 
 async function main(){
-    // Call the function to get the 5-day weather forecast
-    const returned_data = await getWeatherForecastThreeHour('New York');
+    const returned_data = await getWeatherForecastDaily('New York');
     console.log(JSON.stringify(returned_data, null, 4)); 
-    // const cleaned_data =  createForcastObj(returned_data);
-    // console.log(JSON.stringify(cleaned_data, null, 4)); 
 
 }
+
 main();
