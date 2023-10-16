23e81ab2c3cd3678c3aed3ddfbc79d90ea057e5e
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2023-10-16 15:03:39 +0100

    Update app.js

--- front-end/app.js
+++ front-end/app.js
@@ -1,6 +1,27 @@
+const api = "https//:localhost3001/";
 const app = Vue.createApp({
-    // this is the component that controls app section
-    template: '<h2>I am the template</h2>'
+    data(){
+        
+        return {
+            name: 'cian' ,
+            age: 22,
+            showResults: true
+        }
+    },
+
+    methods: {
+        changeTitle(title){
+            console.log("change title pressed")
+            this.name = title
+        },
+        getWeather(city){
+            console.log("Calling API with city" + city)
+        },
+        toggleShowResults(){
+            this.showResults = !this.showResults
+            console.log("show results has been pressed")
+        }
+    }
 });
 
 app.mount('#app')
